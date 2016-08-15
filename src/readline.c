abc507f10de5060e9ca7f43852f0846a986449bf
Hans Lub <hanslub42@gmail.com>
2016-08-15 14:45:03 +0200

    history_list() can return NULL, would casue a segfault

--- src/readline.c
+++ src/readline.c
@@ -625,6 +625,8 @@ direct_keypress(int UNUSED(count), int key)
 
 static char* entire_history_as_one_string(void) {
   HIST_ENTRY **the_list = history_list(), **entryp;
+  if (!the_list) /* i.e. if there is no history */
+    return mysavestring("");
   char *big_string = mymalloc(history_total_bytes() + history_length + 1);
   char * stringp =  big_string;
   for (entryp = the_list; *entryp; entryp++) {
