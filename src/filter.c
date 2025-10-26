90706129888254d51655f5c34cf293f5e0a79891
Hans Lub <hanslub42@gmail.com>
2025-10-26 12:24:04 +0100

    typo causing compile error with --enable-debug

--- src/filter.c
+++ src/filter.c
@@ -146,11 +146,11 @@ void spawn_filter(const char *filter_commandline) {
     if (scan_metacharacters(filter_commandline_full_path, "'|\"><$`"))  { /* if filter_commandline contains shell metacharacters, let the shell unglue them */
       char *exec_commandline = add3strings("exec", " ", filter_commandline_full_path);
       argv = list4("/bin/sh", "-c", exec_commandline, NULL);
-      DPRINTF1(DEBUG_FILTERING, "exec_commandline = <%s>", exec_command);
+      DPRINTF1(DEBUG_FILTERING, "exec_commandline = <%s>", exec_commandline);
 
     } else {                                              /* if not, split and feed to execvp directly (cheaper, better error message) */
       argv = split_with(filter_commandline_full_path, " ");
-    }   
+    }    
     assert(argv[0]);    
     if(execv(argv[0], argv) < 0) {
       char *sorry = add3strings("Cannot exec filter '", argv[0], add2strings("': ", strerror(errno))); 
